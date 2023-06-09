3374113f51db04010e5e630a3c822a174a6a18f0
Thiago <91092364+thag0@users.noreply.github.com>
2023-06-08 21:53:16 -0300

    Delete entidade directory

--- entidade/Poco.java
+++
@@ -1,9 +0,0 @@
-package entidade;
-public class Poco extends Entidade {
-    public Poco(int posX, int posY){
-        this.simbolo = "P";
-        this.sensacao = "Brisa";
-        this.posX = posX;
-        this.posY = posY;
-    }
-}
